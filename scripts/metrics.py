657eb5920636ceae253ec308a103f2d09a42d296
XiaohangZ <731092302@qq.com>
2023-06-14 00:57:45 +0200

    update

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -1,2 +1,11 @@
+from math import sqrt
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
 def accuracy(predictions,labels):
-    pass
+    print("mean_absolute_error:", mean_absolute_error(predictions, labels))
+    print("mean_squared_error:", mean_squared_error(predictions, labels))
+    print("rmse:", sqrt(mean_squared_error(predictions, labels)))
+    print("r2 score:", r2_score(predictions, labels))
+
