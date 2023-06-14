a784056852f7018e344bd6c6ca978670abc9e534
XiaohangZ <731092302@qq.com>
2023-06-14 11:40:26 +0200

    update

--- datasets/autopilot/AutopilotData.py
+++ datasets/autopilot/AutopilotData.py
@@ -26,20 +26,21 @@ class autopilot_dataset:
         t_list=[]
         t=0
 
+        T_dotr = Fraction(221,5)
+        T_U_dotr = -Fraction(662,45)
+        T_U2_dotr = Fraction(449,180)
+        T_U3_dotr = -Fraction(193,1620)
+        K_delta = -Fraction(7,100)
+        K_U_delta = Fraction(1,360)
+        K_U2_delta = Fraction(1,180)
+        K_U3_delta = -Fraction(1,3024)
+        N_r = 1
+        N_r3 = Fraction(1,2)
+        N_U_r3 = -Fraction(43,180)
+        N_U2_r3 = Fraction(1,18)
+        N_U3_r3 = -Fraction(1,324)
+        
         while t < T:
-            T_dotr = Fraction(221,5)
-            T_U_dotr = -Fraction(662,45)
-            T_U2_dotr = Fraction(449,180)
-            T_U3_dotr = -Fraction(193,1620)
-            K_delta = -Fraction(7,100)
-            K_U_delta = Fraction(1,360)
-            K_U2_delta = Fraction(1,180)
-            K_U3_delta = -Fraction(1,3024)
-            N_r = 1
-            N_r3 = Fraction(1,2)
-            N_U_r3 = -Fraction(43,180)
-            N_U2_r3 = Fraction(1,18)
-            N_U3_r3 = -Fraction(1,324)
 
             r = 0 
             F_rudder = K_delta*delta + K_U_delta*U*delta + K_U2_delta*math.pow(U,2)*delta + K_U3_delta*math.pow(U,3)*delta
