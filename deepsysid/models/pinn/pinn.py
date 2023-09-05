6b2a6c6ede61a22c7c98b6a83e9852247f33b67f
XiaohangZ <731092302@qq.com>
2023-09-05 23:23:15 +0200

    update

--- deepsysid/models/pinn/pinn.py
+++ deepsysid/models/pinn/pinn.py
@@ -24,6 +24,38 @@ class PINNNet(nn.Module):
         out3 = self.Linear2(out2)
         return out3
 
+class PILSTMNet(nn.Module):
+    def __init__(self, inputNode=2, hiddenNode=256, outputNode=1):
+        super(PILSTMNet, self).__init__()
+
+        self.inp = nn.Linear(inputNode, hiddenNode)
+        self.rnn = nn.LSTM(hiddenNode, hiddenNode, 2)  # input,hidden,layers
+        # self.rnn = nn.RNN(hiddenNode , hiddenNode , 3)
+        self.out = nn.Linear(hiddenNode, 1)
+
+        self.outputNode = outputNode
+
+    def step(self, input, hidden=None):
+        input = self.inp(input).unsqueeze(1)
+        output, hidden = self.rnn(input, hidden)
+        output = self.out(output.squeeze(1))
+        return output, hidden
+
+    def forward(self, inputs, steps=40, hidden=None):
+        # format: batch, seq, features
+        outputs = torch.zeros(inputs.shape[0], inputs.shape[1], self.outputNode)
+
+        for i in range(steps):
+            input = inputs[:, i]
+
+            # print(input.shape)
+            output, hidden = self.step(input, hidden)
+
+            # print(output.shape)
+            outputs[:, i] = output
+
+        return outputs
+
 def pinn_loss(r_true, r_pred, delta, U, r_prv):
     T_dotr = 221/5
     T_U_dotr = -62/45
